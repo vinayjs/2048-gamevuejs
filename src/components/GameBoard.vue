649675b45a0557cc7abd4a6b876033586f57c44f
vinay chandran <vinay.sarvodaya1991@gmail.com>
2022-12-02 21:00:36 +0530

    add id based on row-column indexes

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,17 +1,21 @@
 <template>
    <div class="game-board"> 
-    <div class="board x2">2</div>
-    <div class="board">0</div>
-    <div class="board">0</div>
-    <div class="board">0</div>
-    <div class="board">0</div>
-    <div class="board">0</div>
-    <div class="board x8">8</div>
-    <div class="board">0</div>
-    <div class="board">0</div>
-    <div class="board">0</div>
-    <div class="board x4">4</div>
-    <div class="board">0</div>
+    <div class="board x2" id="0-0">1</div>
+    <div class="board" id="0-1">2</div>
+    <div class="board" id="0-2">3</div>
+    <div class="board" id="0-3">4</div>
+    <div class="board" id="1-0">5</div>
+    <div class="board" id="1-1">6</div>
+    <div class="board" id="1-2">7</div>
+    <div class="board x8" id="1-3">8</div>
+    <div class="board" id="2-0">9</div>
+    <div class="board" id="2-1">10</div>
+    <div class="board" id="2-2">11</div>
+    <div class="board" id="2-3">12</div>
+    <div class="board" id="3-0">13</div>
+    <div class="board" id="3-1">14</div>
+    <div class="board" id="3-2">15</div>
+    <div class="board" id="3-3">16</div>
    </div>
 </template>
 
@@ -47,13 +51,56 @@ export default {
  }
  .x2 {
   background-color: yellow;
+  color: white;
  }
 
  .x4 {
   background-color: rgb(222, 156, 238);
+  color: white;
  }
 
  .x8 {
   background-color: rgb(158, 237, 146);
+  color: white;
  }
+ 
+.x16{
+    background-color: #f59575;
+    color: white;
+}
+
+.x32{
+    background-color: #f57c5f;
+    color: white;
+}
+
+.x64{
+    background-color: #f65d3b;
+    color: white;
+}
+
+.x128{
+    background-color: #edce71;
+    color: white;
+}
+
+.x256{
+    background-color: #edcc63;
+    color: white;
+}
+
+.x512{
+    background-color: #edc651;
+    color: white;
+}
+
+.x1024{
+    background-color: #eec744;
+    color: white;
+}
+
+.x2048{
+    background-color: #ecc230;
+    color: white;
+}
 </style>
