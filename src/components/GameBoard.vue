fbab130da56a4042c07336c90c906d34ec1c2f8e
vinay chandran <vinay.sarvodaya1991@gmail.com>
2022-12-10 13:44:27 +0530

    refactored right move function

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -191,8 +191,8 @@ export default {
           }
           arr[i + 1] = arr[i] + arr[i + 1];
           arr[i] = "";
-          if(i+2 <= arr.length && arr[i+2]===""){
-           [arr[i+1],arr[i+2]] = [arr[i+2],arr[i+1]]
+          if(i-1 >= 0 && arr[i-1]!==""){
+           [arr[i-1],arr[i]] = [arr[i],arr[i-1]]
           }
           break;
         }
